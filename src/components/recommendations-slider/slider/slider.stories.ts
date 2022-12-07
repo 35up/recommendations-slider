444798f50307ff854bd29d99192a066b56f3b72d
Fausto Arturo Guerrero Velez <enjoythelive1@gmail.com>
2022-12-07 14:31:29 +0100

    UP-3874 - Adds storybook

---
+++ src/components/recommendations-slider/slider/slider.stories.ts
@@ -0,0 +1,24 @@
+import { html } from 'lit';
+import { Meta, Story } from '@storybook/web-components';
+import { arrayRange } from '@35up/tslib-utils';
+import './slider';
+
+
+export default {
+  title: 'Components/Slider',
+} as Meta;
+
+export const Default: Story = () => html`
+  <up-slider>${(arrayRange(0, 20)
+    .map(n => html`
+    <div
+      style="
+        width: 200px;
+        height: 350px;
+        background-color: hsl(${n / 20}turn, 70%, 50%);
+      "
+    ></div>
+  `))}
+  </up-slider>
+`;
+
