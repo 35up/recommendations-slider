fa193a0f082d88b61519516373fe551cd4f6de60
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2023-02-01 17:48:35 +0000

    UP-4018 - Migrates to vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,32 @@
+const { mergeConfig } = require('vite');
+
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
   ],
-  "framework": "@storybook/web-components"
-}
+  framework: '@storybook/web-components',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    config.optimizeDeps.include = [
+      ...(config.optimizeDeps?.include ?? []),
+      '@storybook/web-components',
+    ]
+    config.optimizeDeps.exclude = [...(config.optimizeDeps?.exclude ?? []), 'lit', 'lit-html']
+
+    return mergeConfig(config, {
+      resolve: {
+        alias: [
+          { find: /^lit\/([\w-/]+)$/, replacement: 'lit/$1.js' },
+        ],
+      },
+    });
+  },
+};
