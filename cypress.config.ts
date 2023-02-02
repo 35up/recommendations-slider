c4d46290e77e9acfb92d686a729f22ef6b2d3f72
Fausto Arturo Guerrero Velez <enjoythelive1@gmail.com>
2023-02-02 13:05:11 +0100

    UP-4015 - Renames cypress test files

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   screenshotOnRunFailure: false,
   e2e: {
     supportFile: false,
+    specPattern: 'cypress/e2e/*.spec.*',
     setupNodeEvents(on) {
       on('before:run', () => {
         exec('npm run storybook -- --ci');
