007cac1efcc2e6817c45bac057813bab51567294
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2023-04-12 18:59:08 +0100

    UP-4398 - Adds custom style story

--- packages/react-slider/src/components/slider.stories.tsx
+++ packages/react-slider/src/components/slider.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Style } from 'react-style-tag';
 import { RecommendationsSlider } from './slider';
 
 
@@ -28,3 +29,65 @@ export default meta;
 type Story = StoryObj<typeof RecommendationsSlider>;
 
 export const Default: Story = {};
+
+export const CustomStyles: Story = {
+  render(props) {
+    return (
+      <>
+        <Style>{`
+          .custom-slider {
+            --recommendation-height: 30rem;
+            font-size: 14px;
+            color: #333;
+          }
+
+          .custom-slider::part(recommendation) {
+            border-radius: 10px;
+            background: #eaf1fb;
+            padding: 10px;
+          }
+
+          .custom-slider::part(button) {
+            border-radius: 999px;
+            background: #d3e3fd;
+          }
+
+          .custom-slider::part(button):hover {
+            background: #b4cff7;
+          }
+
+          .custom-slider::part(price) {
+            font-weight: bold;
+            color: #555;
+          }
+
+          .custom-slider::part(arrow) {
+            color: #d3e3fd;
+          }
+
+          .custom-slider::part(arrow):hover {
+            color: #b4cff7;
+          }
+
+          button {
+            margin: 0 10px;
+            background: transparent;
+            color: #d3e3fd;
+            border: 1px solid #d3e3fd;
+            border-radius: 999px;
+            cursor: pointer;
+          }
+
+          button:hover {
+            color: #b4cff7;
+            border: 1px solid #b4cff7;
+          }
+        `}</Style>
+        <RecommendationsSlider {...props} className="custom-slider">
+          <button slot="arrow-left">{'<'}</button>
+          <button slot="arrow-right">{'>'}</button>
+        </RecommendationsSlider>
+      </>
+    );
+  },
+};
