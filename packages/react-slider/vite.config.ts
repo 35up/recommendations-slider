da8649e74da1a119924adbfe9d6c3d2f55c1f42a
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2023-04-13 10:22:25 +0100

    UP-4398 - Defines build for react-slider

--- packages/react-slider/vite.config.ts
+++ packages/react-slider/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
+import { basename, dirname } from 'path';
 import react from '@vitejs/plugin-react';
+import pkg from './package.json';
 
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      formats: ['esm'],
+      fileName: basename(pkg.module, '.esm.js'),
+    },
+    outDir: dirname(pkg.module),
+    sourcemap: true,
+  },
   test: {
     environment: 'jsdom',
   },
