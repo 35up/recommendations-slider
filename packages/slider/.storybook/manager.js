e1d18371eef8a5e703aaad5bdb8cc0fb28d28c6c
drafter500 <vzemtsov@yahoo.com>
2023-04-25 09:40:17 +0200

    UP-4369 - Fixes path to the logo

--- packages/slider/.storybook/manager.js
+++ packages/slider/.storybook/manager.js
@@ -1,11 +1,12 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming';
+import logo from './logo-colorful.svg';
 
 
 const theme = create({
   base: 'light',
   brandTitle: '35up Recommendations Slider Examples',
-  brandImage: '/logo-colorful.svg',
+  brandImage: logo,
   brandTarget: '_self',
 });
 
